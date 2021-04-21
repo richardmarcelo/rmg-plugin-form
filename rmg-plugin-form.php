78e92a5691f42ef557f9f20b9676498ddba8120b
richardmarcelo <richardmarcelo.it@gmail.com>
2021-04-20 22:34:59 -0400

    Crea la tabla y graba los datos desde el formulario

--- rmg-plugin-form.php
+++ rmg-plugin-form.php
@@ -222,14 +222,12 @@ function RMG_Plugin_admin() {
  */
 function RMG_Obtener_IP_usuario(){
 
-    foreach (array('HTTP_CLIENT_IP', 'HTTP_X_FORWARDED_FOR', 'HTTP_X_FORWARDED', 'HTTP_X_CLUSTER_CLIENT_IP', 'HTTP_FORWARDED_FOR', 'HTTP_FORWARDED', 'REMOTE_ADDR') as $key)
-    {
-        if (array_key_exists($key, $_SERVER) === true)
-        {
-            foreach (array_map('trim', explode(',', $_SERVER[$key])) as $ip)
-            {
-                if (filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_NO_PRIV_RANGE | FILTER_FLAG_NO_RES_RANGE) !== false)
-                {
+    foreach (array('HTTP_CLIENT_IP', 'HTTP_X_FORWARDED_FOR', 'HTTP_X_FORWARDED',
+        'HTTP_X_CLUSTER_CLIENT_IP', 'HTTP_FORWARDED_FOR', 'HTTP_FORWARDED',
+        'REMOTE_ADDR') as $key) {
+        if (array_key_exists($key, $_SERVER) === true) {
+            foreach (array_map('trim', explode(',', $_SERVER[$key])) as $ip) {
+                if (filter_var($ip, FILTER_VALIDATE_IP) !== false) {
                     return $ip;
                 }
             }
